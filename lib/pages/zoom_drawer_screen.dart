a1b0c087b2c33ee446a60eb0e2cceeba2125cd3c
Faysal-Maxamed <pheymoha17@gmail.com>
2025-01-17 13:38:37 +0300

    Zoom drawer added

---
+++ lib/pages/zoom_drawer_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_zoom_drawer/flutter_zoom_drawer.dart';
+
+class ZoomDrawerScreen extends StatelessWidget {
+  const ZoomDrawerScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
