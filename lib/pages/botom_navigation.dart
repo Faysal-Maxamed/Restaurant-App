9858e448c0deb5433a6e2626306c1516ac76e1cb
Faysal-Maxamed <pheymoha17@gmail.com>
2025-01-17 14:21:09 +0300

    bottom navigation created

--- lib/pages/botom_navigation.dart
+++ lib/pages/botom_navigation.dart
@@ -1,17 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:resturent_app/pages/home.dart';
 
-class BotomNavigationScreen extends StatelessWidget {
+class BotomNavigationScreen extends StatefulWidget {
   const BotomNavigationScreen({super.key});
 
+  @override
+  State<BotomNavigationScreen> createState() => _BotomNavigationScreenState();
+}
+
+class _BotomNavigationScreenState extends State<BotomNavigationScreen> {
+  int currentindex = 0;
+  List <Widget>pages = [
+    HomeScreen()
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Text(""),
-      bottomNavigationBar: BottomNavigationBar(items: [
-        BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
-        BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
-        BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
-      ]),
+      bottomNavigationBar: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          currentIndex: currentindex,
+          onTap: (value) {
+            setState(() {
+              currentindex = value;
+            });
+          },
+          items: [
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.shopping_cart_outlined), label: "Cart"),
+            BottomNavigationBarItem(icon: Icon(Icons.school), label: "Book"),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.person_3_sharp), label: "Profile"),
+          ]),
     );
   }
 }
